70d49eee71a65d393f54049eb5cec582d6b4c62f
Broveku <gearhearjordan@gmail.com>
2021-07-12 16:05:33 -0700

    finished the days task

--- src/components/Programmers.js
+++ src/components/Programmers.js
@@ -28,28 +28,24 @@ export default function Programmers() {
   // We'll have to use the state hook twice, as we need two slices of state.
   // The programmers list on the one hand, and the id of the featured programmer on the other.
   const [programmers, setProgrammers] = useState(listOfAwesome)
-  const [programmerID, setProgrammerId] = useState('')
+  const [programmerID, setProgrammerId] = useState(null)
 
   const getNameOfFeatured = () => {
-    programmers.forEach(programmer => {
-      if (programmer.id === programmerID){
-        return programmer.name
-      }
-    })
-
+    return programmers.find((obj) => obj.id === programmerID).name
+    
 
     // Leave this for last!
     // This is NOT an event handler but a helper function. See its usage inside the JSX.
     // It's going to utilize both slices of state to return the _name_ of the featured dev.
     // The beauty of closures is that we can "see" both slices of state from this region
     // of the program, without needing to inject the information through arguments.
-
-  };
+  
+};
 
   const style = {
     fontSize: '1.5em',
     marginTop: '0.5em',
-    color: 'royalblue', // 🤔 color turns to gold, when celebrating
+    color: programmerID ? 'gold' : 'royalblue', // 🤔 color turns to gold, when celebrating
   };
 
   return (
@@ -62,7 +58,8 @@ export default function Programmers() {
           we could never add or edit programmers in the future. The list would be a static thing." */
           listOfAwesome.map(dev =>
             <div className='programmer' key={dev.id}>
-              {dev.name} <button onClick={() => {dev.id}}>Feature</button>
+              {dev.name} <button onClick={() => {
+                setProgrammerId(dev.id)}}>Feature</button>
             </div>
           )
         }
@@ -72,7 +69,7 @@ export default function Programmers() {
           // Ternaries are fantastic to render "one thing or the other" depending on the "truthiness" of something.
           // Pseudo-code: if the currently featured id is truthy render text 1, otherwise render text 2.
           // Replace the hard-coded false with the correct variable.
-          false
+          programmerID
             ? `🎉 Let's celebrate ${getNameOfFeatured()}! 🥳`
             : 'Pick an awesome programmer'
         }
